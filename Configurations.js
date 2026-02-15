d8887d0e16416960b28abc72fa06214079e2d511
Alex-code-prog <acelester698@gmail.com>
2026-02-15 04:05:43 +0100

    Update owner and sessionId configurations

--- Configurations.js
+++ Configurations.js
@@ -6,9 +6,9 @@ if (!gg) {
 }
 
 
-global.owner = gg.split(",");
+global.owner = gg.split("2348032180744");
 global.mongodb = process.env.MONGODB || "Put your alternative mongodb url here";
-global.sessionId = process.env.SESSION_ID || "ok";
+global.sessionId = process.env.SESSION_ID || "Levanter/juggernaut123@$";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
 global.packname = process.env.PACKNAME || `Atlas Bot`;
